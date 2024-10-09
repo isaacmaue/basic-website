fce2a148f5e92576cc6b473ee633e22a6690467b
Isaac Maue <isaac.maue@protonmail.com>
2024-10-09 14:36:16 -0500

    First commit

---
+++ src/main/java/com/example/basic_website/BasicWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.basic_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basic_website/BasicWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basic_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
